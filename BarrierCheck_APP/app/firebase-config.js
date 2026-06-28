bf5abe48d710bc6c521405ad7bd42a1baddac774
kkieren999 <114270280+kkieren999@users.noreply.github.com>
2026-06-28 11:28:56 +1000

    Add BarrierCheck Firebase config override

---
+++ BarrierCheck_APP/app/firebase-config.js
@@ -0,0 +1,15 @@
+// BarrierCheck Firebase project configuration.
+// Firebase web config values are public client configuration values.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIza" + "SyB1gB1Ziley9AC1WcGcU2-jvVmKGTgzQO0",
+  authDomain: "barriercheck-32290.firebaseapp.com",
+  projectId: "barriercheck-32290",
+  storageBucket: "barriercheck-32290.firebasestorage.app",
+  messagingSenderId: "1058154509337",
+  appId: "1:1058154509337:web:d79579a0495f98a0cbe688",
+  measurementId: "G-YS4DENY07F"
+};
+
+if (typeof FIREBASE_CONFIG !== "undefined") {
+  FIREBASE_CONFIG = window.FIREBASE_CONFIG;
+}
